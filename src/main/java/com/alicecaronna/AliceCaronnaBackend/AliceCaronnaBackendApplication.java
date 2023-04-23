f45c2c76a16fc1dbbc500bf1b7cfb27c4ded0eff
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2023-04-23 15:42:39 +0200

    Init

---
+++ src/main/java/com/alicecaronna/AliceCaronnaBackend/AliceCaronnaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alicecaronna.AliceCaronnaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliceCaronnaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AliceCaronnaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alicecaronna/AliceCaronnaBackend/AliceCaronnaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alicecaronna.AliceCaronnaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliceCaronnaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
